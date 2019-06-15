be8e1e37302b3b8bd1f8d6165cb54309e3483967
Ravi Autar <raviautar27@gmail.com>
2019-06-15 22:58:49 +0000

    Add serving historic data

--- main.py
+++ main.py
@@ -16,19 +16,23 @@ def read_data(household=1, typehh=0):
         data['date'] = dt
         data = data.set_index('date')
 
-        data = data.resample('H').mean()
-        print(type(data))
-        return data[-672:]
+        data = data.resample('D').mean()
+        return data[-30:]
     else:
         data = pd.read_csv('useful_solar/run_household_solar_{}/power2016Household.csv'.format(household), usecols=[2])
+        sol = pd.read_csv('useful_solar/run_household_solar_{}/power2016_solar_module.csv'.format(1), usecols=[3])
+        hh = pd.read_csv('useful_solar/run_household_solar_{}/gridbalance2016.csv'.format(1), usecols=[3])
         data.columns = ['PowerConsumed']
+        data['Produced'] = sol.values + hh.values
         base = datetime(2016, 1, 1)
         dt = [base + timedelta(minutes=x) for x in range(len(data))]
         data['date'] = dt
         data = data.set_index('date')
+        data = data[data.index > datetime(2016, 6, 1)]
+        data = data[data.index < datetime(2016, 7, 1)]
 
-        data = data.resample('H').mean()
-        return data[-672:]
+        data = data.resample('D').mean()
+        return data[-30:]
 
 
 app = Flask(__name__) #create the Flask app
