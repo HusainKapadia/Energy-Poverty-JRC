af4c413b44d9a905ca2dd096d3bdaa983914aaaf
Ravi Autar <raviautar27@gmail.com>
2019-06-15 18:35:58 +0200

    Update

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from flask import Flask, request, json #import main Flask class and request object
 from forex_python.converter import CurrencyRates
 from flask_cors import CORS
+from datetime import timedelta
 from datetime import datetime
 import numpy as np
+import pandas as pd
 
 
+def read_data(household=1, typehh=0):
+    if not typehh:
+        data = pd.read_csv('useful_no_solar/run_household_{}/power2016Household.csv'.format(household), usecols=1)
+        base = datetime(2016, 1, 1)
+        dt = [base + timedelta(minutes=x) for x in range(len(data))]
+        return dt[-10:]
+
+        # total = total.to_frame()
+        # total['date'] = dt
+        
 app = Flask(__name__) #create the Flask app
 CORS(app)
 
-@app.route('/submissions')
-def jsonexample():
-    return json.dumps([])
-
+@app.route('/consumer_history')
+def hhexample():
+    return json.dumps(read_data())
 
 @app.route('/newsubmit', methods = ['POST'])
 def api_message():
