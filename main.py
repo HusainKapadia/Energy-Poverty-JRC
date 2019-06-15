9d531e69dd30684b6608e16efb945fb0912f1ada
Ravi Autar <raviautar27@gmail.com>
2019-06-15 19:54:15 +0000

    Update for consumer and production data

--- main.py
+++ main.py
@@ -9,20 +9,38 @@ import pandas as pd
 
 def read_data(household=1, typehh=0):
     if not typehh:
-        data = pd.read_csv('useful_no_solar/run_household_{}/power2016Household.csv'.format(household), usecols=1)
+        data = pd.read_csv('useful_no_solar/run_household_{}/power2016Household.csv'.format(household), usecols=[2])
+        data.columns = ['PowerConsumed']
         base = datetime(2016, 1, 1)
         dt = [base + timedelta(minutes=x) for x in range(len(data))]
-        return dt[-10:]
+        data['date'] = dt
+        data = data.set_index('date')
 
-        # total = total.to_frame()
-        # total['date'] = dt
-        
+        data = data.resample('H').mean()
+        print(type(data))
+        return data[-672:]
+    else:
+        data = pd.read_csv('useful_solar/run_household_solar_{}/power2016Household.csv'.format(household), usecols=[2])
+        data.columns = ['PowerConsumed']
+        base = datetime(2016, 1, 1)
+        dt = [base + timedelta(minutes=x) for x in range(len(data))]
+        data['date'] = dt
+        data = data.set_index('date')
+
+        data = data.resample('H').mean()
+        return data[-672:]
+
+
 app = Flask(__name__) #create the Flask app
 CORS(app)
 
 @app.route('/consumer_history')
-def hhexample():
-    return json.dumps(read_data())
+def consumer_history():
+    return read_data().to_json()
+
+@app.route('/producer_history')
+def producer_history():
+    return read_data(typehh=1).to_json()
 
 @app.route('/newsubmit', methods = ['POST'])
 def api_message():
