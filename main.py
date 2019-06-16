62f6719920fdf1d06e2c97d0e7a89b728ef77cc7
Ravi Autar <raviautar27@gmail.com>
2019-06-16 04:12:03 +0200

    Update for retrieving older datapoints

--- main.py
+++ main.py
@@ -83,28 +83,21 @@ def read_data(household=1, typehh=0):
     if not typehh:
         data = pd.read_csv('useful_no_solar/run_household_{}/power2016Household.csv'.format(household), usecols=[2])
         data.columns = ['PowerConsumed']
-        base = datetime(2016, 1, 1)
-        dt = [base + timedelta(minutes=x) for x in range(len(data))]
-        data['date'] = dt
-        data = data.set_index('date')
-
-        data = data.resample('D').mean()
-        return data[-30:]
     else:
         data = pd.read_csv('useful_solar/run_household_solar_{}/power2016Household.csv'.format(household), usecols=[2])
         sol = pd.read_csv('useful_solar/run_household_solar_{}/power2016_solar_module.csv'.format(1), usecols=[3])
         hh = pd.read_csv('useful_solar/run_household_solar_{}/gridbalance2016.csv'.format(1), usecols=[3])
         data.columns = ['PowerConsumed']
         data['Produced'] = sol.values + hh.values
-        base = datetime(2016, 1, 1)
-        dt = [base + timedelta(minutes=x) for x in range(len(data))]
-        data['date'] = dt
-        data = data.set_index('date')
-        data = data[data.index > datetime(2016, 6, 1)]
-        data = data[data.index < datetime(2016, 7, 1)]
-
-        data = data.resample('D').mean()
-        return data[-30:]
+    base = datetime(2016, 1, 1)
+    dt = [base + timedelta(minutes=x) for x in range(len(data))]
+    data['date'] = dt
+    data = data.set_index('date')
+    t = datetime.now()
+    data.index = data.index + timedelta(days=(3*365) + 1) # Shift values 3 years
+    data = data[data.index < t]
+    data = data.resample('D').mean()
+    return data[-7:]
 
 # Readjust times
 all_production = all_productionf()
