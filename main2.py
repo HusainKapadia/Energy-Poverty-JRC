3e84f6476a987588470932b879286931cc2b95bf
Ravi Autar <raviautar27@gmail.com>
2019-09-08 16:29:33 +0200

    Add model to back end serving coverage data

--- main2.py
+++ main2.py
@@ -8,11 +8,43 @@ import numpy as np
 import pandas as pd
 import glob
 import random
+import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from xgboost import XGBRegressor
 
-random.seed(42)
 
-prosumers = ['London Webster', 'Quincy Brooks', 'Jazmine Barry', 'Megan Knight', 'Journey Dyer', 'Jazmin Glover', 'Alison Zimmerman', 'Tommy Hale', 'Kobe Fields', 'Jamison Davila', 'Areli James', 'Celeste Russo', 'Kaleb Cole', 'Braden Woodward', 'Rene Bradshaw', 'Brycen Mcknight', 'Alexandria Wood', 'Kaylyn Harper', 'Ean Mullins', 'Cindy Yu']
-consumersp = [random.randint(2, 8) for _ in range(20)]
+def infer_monthly_usage():
+    # Load dataset
+    fulldata = pd.read_pickle('prod/weather_prod_cons.pkl')
+    n_prosumers = 20
+    X = fulldata.copy().dropna()
+    y_prod = X.OverallProd
+    y_cons = X.Consumption
+    X = X.drop(columns=['OverallProd', 'Consumption'])
+
+    # Prepare Data, Train XgBoost Regressor and Fit model
+    xtrain, xtest, ytrain, ytest = train_test_split(X, y_prod, shuffle=False)
+    reg_prod = XGBRegressor()
+    reg_prod.fit(xtrain.values, ytrain)
+    prod_preds = pd.Series(reg_prod.predict(fulldata.drop(columns=['OverallProd', 'Consumption']).values), index=fulldata.index)
+
+    xtrain, xtest, ytrain, ytest = train_test_split(X, y_cons, shuffle=False)
+    reg_cons = XGBRegressor()
+    reg_cons.fit(xtrain.values, ytrain)
+    cons_preds = pd.Series(reg_cons.predict(fulldata.drop(columns=['OverallProd', 'Consumption']).values), index=fulldata.index)
+
+    # Resample to give prediction per month
+    res = pd.DataFrame({'Production': prod_preds.resample('M', how='sum'), 
+                        'Consumption': cons_preds.resample('M', how='sum')}).sort_index() 
+    res['Deficit'] = res.Consumption - res.Production
+    res['ProsumersNeeded'] =  round(res.Deficit/(res.Production/n_prosumers)).astype(int)
+    res['Coverage'] =  round((res.Production*100)/res.Consumption).astype(int)
+    res.index = res.index.month_name()
+    res = res[['ProsumersNeeded', 'Coverage']]
+    return res
 
 def read_data(household=1, typehh=0):
     if not typehh:
@@ -42,6 +74,11 @@ def read_all_prosumers():
         prosumer_data[prosumers[i-1]] = [sum(p['Produced']), sum(p['PowerConsumed']), consumersp[i-1]]
     return prosumer_data
 
+random.seed(42)
+
+prosumers = ['London Webster', 'Quincy Brooks', 'Jazmine Barry', 'Megan Knight', 'Journey Dyer', 'Jazmin Glover', 'Alison Zimmerman', 'Tommy Hale', 'Kobe Fields', 'Jamison Davila', 'Areli James', 'Celeste Russo', 'Kaleb Cole', 'Braden Woodward', 'Rene Bradshaw', 'Brycen Mcknight', 'Alexandria Wood', 'Kaylyn Harper', 'Ean Mullins', 'Cindy Yu']
+consumersp = [random.randint(2, 8) for _ in range(20)]
+res = infer_monthly_usage().T.to_dict()
 print('Calculating leaderboard...')
 all_production = read_all_prosumers()
 
@@ -57,6 +94,10 @@ def producer_history():
 @app.route('/leaderboard')
 def leaderboard():
     return jsonify(all_production)
+
+@app.route('/coverage')
+def coverage():
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000) #run app in debug mode on port 5000
